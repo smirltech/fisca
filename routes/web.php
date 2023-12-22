0ef3f7c07ad79a48e7f562ed7242eff4818e2915
paul <paulkaf84@gmail.com>
2023-12-22 22:42:57 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [EmployeeController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

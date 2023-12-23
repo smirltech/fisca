35b8d827da4dfcaf56ad21bc8603296909e74d5a
paul <paulkaf84@gmail.com>
2023-12-23 11:04:39 +0200

    fix PublicHolidaySeeder

--- database/seeders/PublicHolidaySeeder.php
+++ database/seeders/PublicHolidaySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\PublicHoliday;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PublicHolidaySeeder extends Seeder
 {
@@ -14,35 +15,48 @@ class PublicHolidaySeeder extends Seeder
     public function run(): void
     {
         // drc public holidays
-
-        PublicHoliday::factory()->create([
+        DB::table('public_holidays')->insert([
             [
                 'date' => '2021-01-01',
                 'description' => 'New Year\'s Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'date' => '2021-01-04',
                 'description' => 'Martyrs of Independence Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'date' => '2021-01-16',
                 'description' => 'Heroes\' Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'date' => '2021-01-17',
                 'description' => 'Heroes\' Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'date' => '2021-05-01',
                 'description' => 'Labour Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'date' => '2021-05-17',
                 'description' => 'Ascension Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'date' => '2021-06-30',
                 'description' => 'Independence Day',
+                'created_at' => now()->format('Y-m-d H:i:s'),
+                'updated_at' => now()->format('Y-m-d H:i:s'),
             ]
         ]);
     }

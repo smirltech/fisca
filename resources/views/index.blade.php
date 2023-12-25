6d3d6d4daf14ba8660924ed34102d03e4273d259
jean luc kawel <20ka208@esisalama.org>
2023-12-25 10:05:45 +0200

    Config Route Salary

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1029,7 +1029,7 @@
                         {{ $employee->phone_number }}
                       </td>
                             <td>
-                                <a href="">
+                                <a href="{{route('compute_salary', ['employee_id' => $employee->id])}}">
                                     <button class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                         Salary
                                     </button>

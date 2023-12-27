f50c5c03e88318c07d6e1cf46e36c10470a1b06e
paul <paulkaf84@gmail.com>
2023-12-27 12:56:03 +0200

    new bulletin de paie

--- resources/views/employee-bulletin.blade.php
+++ resources/views/employee-bulletin.blade.php
@@ -1,112 +1,59 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bulletin de paie</title>
-    {{--    @extends('layouts.app')--}}
-        @vite(['/resources/css/app.css'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="">
-<div class="container mx-auto my-12 px-4 sm:px-8">
-    <div class="flex">
-        <img src="https://via.placeholder.com/150" alt="fakeLogo">
+<body class="bg-gray-100 p-8">
+<div class="max-w-3xl mx-auto bg-white p-8 shadow-md">
+    <!-- Entête -->
+    <div class="text-center mb-8">
+        <h1 class="text-3xl font-bold">Bulletin de paie</h1>
+        <p class="text-gray-500">Entreprise XYZ</p>
     </div>
-    <div class="py-8">
-        <div class="flex mb-4">
-            <h2 class="text-2xl font-semibold leading-tight">Bulletin de paie</h2>
-        </div>
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full leading-normal">
-                <thead>
-                <tr>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Nom complet
-                    </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Salaire mensuel
-                    </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Salaire quotidien
-                    </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Salaire horaire
-                    </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Heures supplémentaires
-                    </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Salaire des heures supplémentaires
-                    </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Salaire total
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $full_name }}</td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $monthly_salary }}</td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $daily_salary }}</td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $hourly_salary }}</td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $total_hours_of_overtime_of_this_month }}</td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $overtime_salary }}</td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $total_salary }}</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+
+    <!-- Informations sur l'employé -->
+    <div class="mb-6">
+        <p class="font-bold">Nom de l'employé: John Doe</p>
+        <p class="text-sm text-gray-500">Poste: Développeur Web</p>
     </div>
-    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-        <table class="min-w-full leading-normal">
-            <tbody>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Nom complet
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $full_name }}</td>
-            </tr>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Salaire mensuel
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $monthly_salary }}</td>
-            </tr>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Salaire quotidien
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $daily_salary }}</td>
-            </tr>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Salaire horaire
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $hourly_salary }}</td>
-            </tr>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Heures supplémentaires
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $total_hours_of_overtime_of_this_month }}</td>
-            </tr>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Salaire des heures supplémentaires
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $overtime_salary }}</td>
-            </tr>
-            <tr>
-                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Salaire total
-                </th>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $total_salary }}</td>
-            </tr>
-            </tbody>
-        </table>
+
+    <!-- Tableau des détails du salaire -->
+    <table class="w-full border-collapse border border-gray-300 mb-6">
+        <thead>
+        <tr class="bg-gray-200">
+            <th class="border p-2">Description</th>
+            <th class="border p-2">Quantité</th>
+            <th class="border p-2">Taux</th>
+            <th class="border p-2">Montant</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="border p-2">Salaire journalier</td>
+            <td class="border p-2">20 jours</td>
+            <td class="border p-2">$100</td>
+            <td class="border p-2">$2000</td>
+        </tr>
+        <tr>
+            <td class="border p-2">Salaire horaire</td>
+            <td class="border p-2">160 heures</td>
+            <td class="border p-2">$15</td>
+            <td class="border p-2">$2400</td>
+        </tr>
+        <!-- Ajoutez d'autres lignes pour les heures supplémentaires, etc. -->
+        <tr class="font-bold">
+            <td class="border p-2" colspan="3">Salaire total</td>
+            <td class="border p-2">$4400</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <!-- Pied de page -->
+    <div class="text-gray-500 text-xs">
+        <p>© 2023 Entreprise XYZ</p>
     </div>
 </div>
 </body>

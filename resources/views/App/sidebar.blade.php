b2896357cb6cb0a9ed0ec2ab05b47e3a96a891c5
paul <paulkaf84@gmail.com>
2023-12-27 19:58:27 +0200

    fix span

--- resources/views/App/sidebar.blade.php
+++ resources/views/App/sidebar.blade.php
@@ -12,12 +12,14 @@
         <ul class="mt-6">
             <li class="relative px-6 py-3">
               <span
-                  class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                  @class([
+                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('index')
+                    ])
                   aria-hidden="true"
               ></span>
                 <a
                     class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                    href="#"
+                    href="{{ route('index') }}"
                 >
                     <svg
                         class="w-5 h-5"
@@ -37,7 +39,7 @@
                 </a>
             </li>
         </ul>
-{{--        desktop--}}
+        {{--        desktop--}}
         <ul>
             <li class="relative px-6 py-3">
                 <a
@@ -62,6 +64,12 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
+              <span
+                  @class([
+                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('ComputeDailySalary')
+                    ])
+                  aria-hidden="true"
+              ></span>
                 <a
                     class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     href="#"

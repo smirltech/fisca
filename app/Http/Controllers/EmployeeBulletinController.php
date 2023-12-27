7d9ff2b26b1f349ea608f6e027261dddde16799a
paul <paulkaf84@gmail.com>
2023-12-27 13:20:02 +0200

    fix bulletin template

--- app/Http/Controllers/EmployeeBulletinController.php
+++ app/Http/Controllers/EmployeeBulletinController.php
@@ -12,7 +12,7 @@ class EmployeeBulletinController extends Controller
         $employee = Employee::find($employee_id);
         $full_name = $employee->full_name;
         $monthly_salary = $employee->salary;
-        $daily_salary = $monthly_salary / 30;
+        $daily_salary = $monthly_salary / 26;
         $hourly_salary = $daily_salary / 8;
         $total_hours_of_overtime_of_this_month = SalaryController::getTotalHoursOfOvertimeOfThisMonth($employee->id);
         $overtime_salary = SalaryController::overtime_salary($hourly_salary, $total_hours_of_overtime_of_this_month);
--- resources/views/employee-bulletin.blade.php
+++ resources/views/employee-bulletin.blade.php
@@ -11,12 +11,12 @@
     <!-- Entête -->
     <div class="text-center mb-8">
         <h1 class="text-3xl font-bold">Bulletin de paie</h1>
-        <p class="text-gray-500">Entreprise XYZ</p>
+        <p class="text-gray-500">SmirlTech</p>
     </div>
 
     <!-- Informations sur l'employé -->
     <div class="mb-6">
-        <p class="font-bold">Nom de l'employé: John Doe</p>
+        <p class="font-bold">Nom de l'employé: {{ $full_name }}</p>
         <p class="text-sm text-gray-500">Poste: Développeur Web</p>
     </div>
 
@@ -31,29 +31,39 @@
         </tr>
         </thead>
         <tbody>
-        <tr>
+        {{--<tr>
             <td class="border p-2">Salaire journalier</td>
-            <td class="border p-2">20 jours</td>
-            <td class="border p-2">$100</td>
-            <td class="border p-2">$2000</td>
-        </tr>
+            <td class="border p-2">26 jours</td>
+            <td class="border p-2">{{ Number::currency($daily_salary) }}</td>
+            <td class="border p-2">{{ Number::currency($monthly_salary) }}</td>
+        </tr>--}}
         <tr>
             <td class="border p-2">Salaire horaire</td>
-            <td class="border p-2">160 heures</td>
-            <td class="border p-2">$15</td>
-            <td class="border p-2">$2400</td>
+            <td class="border p-2">{{ 8 * 26 }} heures</td>
+            <td class="border p-2">{{ Number::currency($hourly_salary) }}</td>
+            <td class="border p-2">{{ Number::currency($hourly_salary * 8 * 26) }}</td>
         </tr>
         <!-- Ajoutez d'autres lignes pour les heures supplémentaires, etc. -->
         <tr class="font-bold">
+            <td class="border p-2" colspan="3">Sous total</td>
+            <td class="border p-2">{{ Number::currency($daily_salary * 26) }}</td>
+        </tr>
+        <tr class="">
+            <td class="border p-2">Heures supplementaires</td>
+            <td class="border p-2">{{ $total_hours_of_overtime_of_this_month }} heures</td>
+            <td class="border p-2">{{ Number::currency($hourly_salary) }}</td>
+            <td class="border p-2">{{ Number::currency(/*$hourly_salary * $total_hours_of_overtime_of_this_month,*/ $overtime_salary) }}</td>
+        </tr>
+        <tr class="font-bold">
             <td class="border p-2" colspan="3">Salaire total</td>
-            <td class="border p-2">$4400</td>
+            <td class="border p-2">{{ Number::currency($total_salary) }}</td>
         </tr>
         </tbody>
     </table>
 
     <!-- Pied de page -->
     <div class="text-gray-500 text-xs">
-        <p>© 2023 Entreprise XYZ</p>
+        <p>© 2023 SmirlTech</p>
     </div>
 </div>
 </body>

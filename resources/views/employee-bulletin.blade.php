74be7803faab6ba0f24f3b4ec2f70156d1f9310d
paul <paulkaf84@gmail.com>
2023-12-27 12:31:28 +0200

    bulletin de paie

--- resources/views/employee-bulletin.blade.php
+++ resources/views/employee-bulletin.blade.php
@@ -1,4 +1,4 @@
- <!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,12 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bulletin de paie</title>
-    @extends('layouts.app')
-{{--    @vite(['/resources/css/app.css'])--}}
-
+    {{--    @extends('layouts.app')--}}
+        @vite(['/resources/css/app.css'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-red-500">
-    <div class="container mx-auto px-4 sm:px-8">
+<body class="">
+<div class="container mx-auto my-12 px-4 sm:px-8">
+    <div class="flex">
+        <img src="https://via.placeholder.com/150" alt="fakeLogo">
+    </div>
     <div class="py-8">
         <div class="flex mb-4">
             <h2 class="text-2xl font-semibold leading-tight">Bulletin de paie</h2>
@@ -56,6 +59,54 @@
                 </tbody>
             </table>
         </div>
+    </div>
+    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <table class="min-w-full leading-normal">
+            <tbody>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Nom complet
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $full_name }}</td>
+            </tr>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Salaire mensuel
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $monthly_salary }}</td>
+            </tr>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Salaire quotidien
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $daily_salary }}</td>
+            </tr>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Salaire horaire
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $hourly_salary }}</td>
+            </tr>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Heures supplémentaires
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $total_hours_of_overtime_of_this_month }}</td>
+            </tr>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Salaire des heures supplémentaires
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $overtime_salary }}</td>
+            </tr>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Salaire total
+                </th>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $total_salary }}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 </body>

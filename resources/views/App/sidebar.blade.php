eb34c2d715c1d23db3b8ae31b3b05c8adaf25863
paul <paulkaf84@gmail.com>
2023-12-28 09:18:50 +0200

    fix current tab highlight

--- resources/views/App/sidebar.blade.php
+++ resources/views/App/sidebar.blade.php
@@ -13,7 +13,7 @@
             <li class="relative px-6 py-3">
               <span
                   @class([
-                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('index')
+                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('index') or request()->routeIs('employees')
                     ])
                   aria-hidden="true"
               ></span>
@@ -42,9 +42,15 @@
         {{--        desktop--}}
         <ul>
             <li class="relative px-6 py-3">
+              <span
+                  @class([
+                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('employees.create')
+                    ])
+                  aria-hidden="true"
+              ></span>
                 <a
                     class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                    href="#"
+                    href="{{ route('employees.create') }}"
                 >
                     <svg
                         class="w-5 h-5"
@@ -66,7 +72,7 @@
             <li class="relative px-6 py-3">
               <span
                   @class([
-                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('ComputeDailySalary')
+                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('salary.details')
                     ])
                   aria-hidden="true"
               ></span>

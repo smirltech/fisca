565599d75f95874f73c944d61643fb5b36efa375
paul <paulkaf84@gmail.com>
2023-12-28 10:11:42 +0200

    fix migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->default('https://picsum.photos/200/300');
             $table->rememberToken();
             $table->timestamps();
         });

c0d940e2a5e463106860314662d5c997cac1ac17
jean luc kawel <20ka208@esisalama.org>
2023-12-29 19:12:07 +0200

    link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
                 <h2
                     class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
                 >
-                    SmirlTech
+                    Dashboard
                 </h2>
                 <!-- CTA -->
                 <a

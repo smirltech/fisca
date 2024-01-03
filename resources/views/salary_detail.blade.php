7efb0b489971f8daa82a6826a4730e2de4b80b0f
paul <paulkaf84@gmail.com>
2024-01-03 11:20:12 +0200

    add CNSS button

--- resources/views/salary_detail.blade.php
+++ resources/views/salary_detail.blade.php
@@ -392,6 +392,13 @@
                         Bulletin
                     </button>
                     </a>
+                    <a href="{{ route('cnss.create', ['employee_id' => $employee->id]) }}">
+                    <button
+                        class="px-4 py-2  text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                    >
+                        CNSS
+                    </button>
+                    </a>
                     @if(!$employee->payed)
                         <a href="{{ route('pay.employee', ['employee_id' => $employee->id]) }}">
                             <button

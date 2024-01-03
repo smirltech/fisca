ff7d385e51c5c81f9dafaae0249229d24df37352
paul <paulkaf84@gmail.com>
2024-01-03 23:39:24 +0200

    fix CNSSFactory

--- database/factories/CNSSDataFactory.php
+++ database/factories/CNSSDataFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CNSSData>
  */
-class CnssDataFactory extends Factory
+class CNSSDataFactory extends Factory
 {
     /**
      * Define the model's default state.

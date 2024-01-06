e3a3edd7f1d69aec7efb535937411891fe271ff2
jean luc kawel <20ka208@esisalama.org>
2024-01-06 09:34:37 +0200

    mail

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -5,20 +5,20 @@
 
     Voici les détails de votre bulletin de paie :
 
-    - **Nom:** {{$name}}
-    - **Prénom:**
-    - **Salaire:**
+    - **First_name:** {{$first_name}}
+    - **Last_name:** {{$last_name}}
+    - **Middle_name:** {{$middle_name}}
+    - **Departement:** {{$departement}}
+    - **Salary:** {{$salary}}
 
-    ---
     **Note:** Vous pouvez télécharger votre bulletin de paie en cliquant sur le bouton ci-dessous.
 
-    ---
-
-    @component('mail::button', ['url' => '/bulletin/download/employee/{employee_id}' ])
+    @component('mail::button', ['url' => '/bulletin/download/employee/3'  ,])
         Télécharger Bulletin de Paie
     @endcomponent
 
 
     Merci,
     L'équipe de paie
+
 @endcomponent

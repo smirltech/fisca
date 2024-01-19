420495392097f3a0a1abb0d3f6f9b4671bf43828
paul <paulkaf84@gmail.com>
2024-01-19 22:54:29 +0200

    completing fields

--- app/Http/Controllers/EmployeeBulletinController.php
+++ app/Http/Controllers/EmployeeBulletinController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CNSSData;
 use App\Models\Employee;
 use Dompdf\Dompdf;
+use Illuminate\Support\Facades\DB;
+use stdClass;
 
 class EmployeeBulletinController extends Controller
 {
@@ -51,6 +54,36 @@ class EmployeeBulletinController extends Controller
         $overtime_salary = SalaryController::overtime_salary($hourly_salary, $total_hours_of_overtime_of_this_month);
         $total_salary = $monthly_salary + $overtime_salary;
 
+        $social_security_number = DB::table('cnss_data')->where('employee_id', '=', $employee->id)->first()->social_security_number;
+
+        // Base salary computation
+        $salary = new stdClass();
+        $salary->hourly = $employee->base_salary;
+        $salary->numberOfHours = 26 * 8;
+        $salary->total = $salary->numberOfHours * $salary->hourly;
+
+        // Base remuneration computation
+        $overtimes = new stdClass();
+        $overtimes->nbOfHours = SalaryController::getTotalHoursOfOvertimeOfThisMonth($employee->id);
+        $coef = 0.01;
+        $overtimes->rate = $salary->hourly * $coef * 100;
+        $overtimes->total = $overtimes->nbOfHours * $overtimes->rate;
+
+        // Supplements and Bonus computation
+        $supp_and_bonus = new stdClass();
+        $coef = 0.003;
+        $supp_and_bonus->weekEnd = $salary->hourly * $coef * 100;
+        $supp_and_bonus->divers = $salary->hourly * $coef * 100;
+        $coef = 0.005;
+        $supp_and_bonus->primes = $salary->hourly * $coef * 100;
+        $supp_and_bonus->total = $supp_and_bonus->weekEnd + $supp_and_bonus->divers + $supp_and_bonus->primes;
+
+        // Avantages et absences
+
+
+
+
+
         return view(
             'employee-bulletin',
             compact(
@@ -63,7 +96,11 @@ class EmployeeBulletinController extends Controller
                 'total_salary',
                 'department',
                 'download',
-                'employee'
+                'employee',
+                'social_security_number',
+                'salary',
+                'overtimes',
+                'supp_and_bonus'
             )
         );
     }
--- resources/views/employee-bulletin.blade.php
+++ resources/views/employee-bulletin.blade.php
@@ -23,10 +23,10 @@
     <!-- Informations sur l'employé -->
     <div class="mb-6">
 
-        <p class="text-sm text-gray-500">N° d'affiliation de l'Employeur à la Sécurité sociale: </p>
-        <p class="text-sm text-gray-500">Nom de l'employeur: </p>
-        <p class="text-sm text-gray-500">Branche d'activité: </p>
-        <p class="text-sm text-gray-500">Feuille de paie du </p>
+        <p class="text-sm text-gray-500">N° d'affiliation de l'Employeur à la Sécurité sociale: {{ $cnss->affiliation_number ?? 'CNSS 0000' }}</p>
+        <p class="text-sm text-gray-500">Nom de l'employeur: SmilrTech </p>
+        <p class="text-sm text-gray-500">Branche d'activité: Developpement de logiciel</p>
+        <p class="text-sm text-gray-500">Feuille de paie du {{ now()->toDateString() }}</p>
     </div>
 
     <!-- Tableau des détails du salaire -->
@@ -45,19 +45,19 @@
 
         <tr>
             <td class="border p-2">Numéro d'ordre</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $employee->order_number ?? '0000' }}</td>
         </tr>
         <tr>
             <td class="border p-2">Nom et Prénoms du Travailleur</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $full_name }}</td>
         </tr>
         <tr>
             <td class="border p-2">Emploi et Catégorie</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $employee->categorie }}</td>
         </tr>
         <tr>
             <td class="border p-2">Numéro d'immatriculation à la sécurité sociale</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $social_security_number }}</td>
         </tr>
 
         <tr>
@@ -67,15 +67,15 @@
         <tr>
             <td class="border p-2" rowspan="3">Salaire</td>
             <td class="border p-2" >Jour, Heure, Mois</td>
-            <td class="border p-2" ></td>
+            <td class="border p-2" >{{ $salary->hourly }}</td>
         </tr>
         <tr>
             <td class="border p-2" >Jour, heure payés à 100%</td>
-            <td class="border p-2" ></td>
+            <td class="border p-2" >{{ $salary->numberOfHours }}</td>
         </tr>
         <tr>
             <td class="border p-2 font-bold" >Total du</td>
-            <td class="border p-2 font-bold" > </td>
+            <td class="border p-2 font-bold" >{{ $salary->total }}</td>
         </tr>
 
         <tr>
@@ -85,15 +85,15 @@
         <tr>
             <td class="border p-2" rowspan="3">Heures supplementaires</td>
             <td class="border p-2" >Nombre d'heures</td>
-            <td class="border p-2" ></td>
+            <td class="border p-2" >{{ $overtimes->nbOfHours }}</td>
         </tr>
         <tr>
             <td class="border p-2" >Taux</td>
-            <td class="border p-2" ></td>
+            <td class="border p-2" >{{ $overtimes->rate }}</td>
         </tr>
         <tr>
             <td class="border p-2 font-bold" >Total du</td>
-            <td class="border p-2 font-bold" > </td>
+            <td class="border p-2 font-bold" >{{ $overtimes->total }}</td>
         </tr>
         <tr>
             <th class=" p-2 bg-gray-200" colspan="3">Suppléments et Bonus</th>
@@ -101,15 +101,15 @@
 
         <tr>
             <td class="border p-2">Supplement du travail du samedi, du dimanche et jours feries</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $supp_and_bonus->weekEnd }}</td>
         </tr>
         <tr>
             <td class="border p-2">Primes</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $supp_and_bonus->primes }}</td>
         </tr>
         <tr>
             <td class="border p-2">Divers</td>
-            <td class="border p-2" colspan="2"></td>
+            <td class="border p-2" colspan="2">{{ $supp_and_bonus->divers }}</td>
         </tr>
 
 

aa7182681f54ba0004ddfaced235a5e778ed351c
jean luc kawel <jeanluckawel45@gmail.com>
2024-02-10 19:37:00 +0000

    delete note

--- resources/views/App/sidebar.blade.php
+++ resources/views/App/sidebar.blade.php
@@ -186,7 +186,7 @@
                             d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
                         ></path>
                     </svg>
-                    <span class="ml-4">Forms</span>
+                    <span class="ml-4">Agent</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
@@ -208,52 +208,7 @@
                             d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
                         ></path>
                     </svg>
-                    <span class="ml-4">Cards</span>
-                </a>
-            </li>
-            <li class="relative px-6 py-3">
-                <a
-                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                    href="charts.html"
-                >
-                    <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-                        ></path>
-                        <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
-                    </svg>
-                    <span class="ml-4">Charts</span>
-                </a>
-            </li>
-            <li class="relative px-6 py-3">
-                <a
-                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                    href="buttons.html"
-                >
-                    <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-                        ></path>
-                    </svg>
-                    <span class="ml-4">Buttons</span>
+                    <span class="ml-4">Details</span>
                 </a>
             </li>
         </ul>

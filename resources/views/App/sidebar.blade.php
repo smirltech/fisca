b47cde581907b223a04ea7c2c7a463b1c3d2cf21
jean luc kawel <jeanluckawel45@gmail.com>
2024-02-10 19:49:53 +0000

    MVP

--- resources/views/App/sidebar.blade.php
+++ resources/views/App/sidebar.blade.php
@@ -103,6 +103,7 @@
                 class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
             >
                {{ Auth::user()->name}}
+
             </button>
         </div>
     </div>

e4558fa895bfc151f9b189b918bd67c91c1fbb5e
paul <paulkaf84@gmail.com>
2024-02-17 20:03:49 +0200

    fix cnss page

--- resources/views/App/sidebar.blade.php
+++ resources/views/App/sidebar.blade.php
@@ -40,6 +40,25 @@
                 </a>
             </li>
         </ul>
+        <ul class="">
+            <li class="relative px-6 py-3">
+              <span
+                  @class([
+                        'absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-purple-600' => request()->routeIs('cnss')
+                    ])
+                  aria-hidden="true"
+              ></span>
+                <a
+                    class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                    href="{{ route('cnss') }}"
+                >
+                    <div class="flex justify-center w-5 h-5">
+                        <span class="material-symbols-outlined">security</span>
+                    </div>
+                    <span class="ml-4">cnss</span>
+                </a>
+            </li>
+        </ul>
         {{--        desktop--}}
         <ul>
             <li class="relative px-6 py-3">

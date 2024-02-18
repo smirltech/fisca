81492b55a63fdbf476865b2b28de051d3f891ecc
Jean luc kawel <20ka208@esisalama.org>
2024-02-18 08:52:05 +0200

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,13 +75,13 @@
 <section class="osahan-main-body osahan-signin-main">
     <div class="container">
         <div class="row d-flex align-items-center justify-content-center vh-100">
-            <div class="landing-page shadow-sm bg-success col-lg-6">
-                <a class="position-absolute btn-sm btn btn-outline-light m-4 zindex" href="home.html">Skip <i class="icofont-bubble-right"></i></a>
+            <div class="landing-page shadow-sm bg-primary col-lg-6">
+                <a class="position-absolute btn-sm btn btn-outline-light m-4 zindex" href="https://smirltech.com">Contact <i class="icofont-bubble-right"></i></a>
                 <div class="osahan-slider m-0">
                     <div class="osahan-slider-item text-center">
                         <div class="d-flex align-items-center justify-content-center vh-100 flex-column">
                             <i class="icofont-sale-discount display-1 text-warning"></i>
-                            <h4 class="my-4 text-white">Best Prices & Offers</h4>
+                            <h4 class="my-4 text-white">{{  ucfirst(config('app.name')) }}</h4>
                             <p class="text-center text-white-50 mb-5 px-4">Cheaper prices than your local<br>supermarket, great cashback offers to top it off.</p>
                         </div>
                     </div>
@@ -92,25 +92,35 @@
                     <div class="p-3">
                         <h2 class="my-0">Welcome Back</h2>
                         <p class="small mb-4">Sign in to Continue.</p>
-                        <form action="https://askbootstrap.com/preview/grofarweb/verification.html">
+
+
+
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email</label>
-                                <input placeholder="Enter Email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <input placeholder="Enter Email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                name="email" value="{{ old('email') }}"
+                                >
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input placeholder="Enter Password" type="password" class="form-control" id="exampleInputPassword1">
+                                <label for="password">Password</label>
+                                <input placeholder="Enter Password" type="password" class="form-control " id="password"
+                                name="password" value="{{ old('password') }}"
+                                >
                             </div>
-                            <button type="submit" class="btn btn-success btn-lg rounded btn-block">Sign in</button>
+                            <div class="block mt-4">
+                            <label for="remember_me" class="inline-flex items-center">
+                                <input id="remember_me" type="checkbox"
+                                       class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                                       name="remember">
+                                <span class="ms-2 text-sm  text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                            </label>
+                        </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary btn-lg rounded btn-block">Sign in</button>
                         </form>
-                        <p class="text-muted text-center small m-0 py-3">or</p>
-                        <a href="verification.html" class="btn btn-dark btn-block rounded btn-lg btn-apple">
-                            <i class="icofont-brand-apple mr-2"></i> Sign up with Apple
-                        </a>
-                        <a href="verification.html" class="btn btn-light border btn-block rounded btn-lg btn-google">
-                            <i class="icofont-google-plus text-danger mr-2"></i> Sign up with Google
-                        </a>
-                        <p class="text-center mt-3 mb-0"><a href="signup.html" class="text-dark">Don't have an account? Sign up</a></p>
+{{--                        <p class="text-center mt-3 mb-0">Don't have an account?<a href="{{ route('') }}" class="text-dark"> Sign up</a></p>--}}
                     </div>
                 </div>
             </div>
